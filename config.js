3e097b99af97daa15622cf41c65a7b785388e59f
Chamosuba <118721343+Chamosuba@users.noreply.github.com>
2023-05-05 19:14:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', '][', true],
-  ['94704461999', '¶', true], 
+  ['94717296778', '][', true],
+  ['94717296778', '¶', true], 
   [''] 
 ] //Number of owners
 
